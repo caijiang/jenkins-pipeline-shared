d7c53d870416a40fcf2be4e4e64d0ed753c0d0e2
Cai Jiang <luffy.ja@gmail.com>
2021-05-20 17:42:01 +0800

    显示更多内容。

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -5,21 +5,34 @@
  */
 def call(String buildStatus = 'STARTED') {
     // build status of null means successful
-    buildStatus =  buildStatus ?: 'SUCCESSFUL'
+    buildStatus = buildStatus ?: 'SUCCESSFUL'
 
 //    def blueOceanDisplayUrlProvider = org.jenkinsci.plugins.displayurlapi.DisplayURLProvider.all().find { it.displayName == "Blue Ocean" }
 //
 //    String url = blueOceanDisplayUrlProvider?.getJobURL(project) ?: env.BUILD_URL
-    String url = (env.BUILD_URL).replaceFirst("/job","/blue/organizations/jenkins").replaceFirst("/job","/detail")
+    String url = (env.BUILD_URL).replaceFirst("/job", "/blue/organizations/jenkins").replaceFirst("/job", "/detail")
+
+    // 更新内容
+    String uls = "<ul>"
+    if (env.BRANCH_NAME != null) {
+        uls = uls + "<li>目标分支：${env.BRANCH_NAME}</li>"
+    }
+    if (env.TAG_NAME != null) {
+        uls = uls + "<li>目标tag：${env.TAG_NAME}</li>"
+    }
+    if (env.CHANGE_URL != null) {
+        uls = uls + "<li>更新内容：${env.CHANGE_URL}</li>"
+    }
+    uls = uls + "</ul>"
 
     // Default values
 //    def color = 'RED'
 //    def colorCode = '#FF0000'
-    GString subject = "${env.PROJECT_NAME} - Build # ${env.BUILD_NUMBER} - ${env.BUILD_STATUS}!"
-//    def summary = "${subject} (${env.BUILD_URL})"
-    GString details = """<summary>${env.PROJECT_NAME} - Build # ${env.BUILD_NUMBER} - ${env.BUILD_STATUS}:</summary>
-<p>Check console output at ${env.BUILD_URL} to view the results.</p>
-<p>Check console output at $url to view the results.</p>"""
+//    env.BUILD_STATUS
+    GString subject = "${env.JOB_NAME} - 构建状态 #${env.BUILD_NUMBER} - $buildStatus!"
+    GString details = """<summary>${env.JOB_NAME} - 构建状态 #${env.BUILD_NUMBER} - ${buildStatus}:</summary>
+$uls
+<p>点击<a href="$url">详情</a>查看包括单元测试结果在内的更多构建内容。</p>"""
 
     // Override default values based on build status
     if (buildStatus == 'STARTED') {
@@ -42,7 +55,7 @@ def call(String buildStatus = 'STARTED') {
 //            recipientProviders: [[$class: 'DevelopersRecipientProvider']]
 //    )
 
-    emailext (
+    emailext(
             recipientProviders: [developers(), requestor()],
             to: 'luffy.ja@gmail.com',
             subject: subject,
